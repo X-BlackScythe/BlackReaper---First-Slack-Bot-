281a6a34a937d6b84ba5a54af04d20037a3b1ced
X-BlackScythe <daviraulinosilva67@gmail.com>
2026-08-25 17:51:35 -0400

    Initial commit

--- Index.js
+++ Index.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 require("dotenv").config();
 
 const { App } = require("@slack/bolt");
@@ -29,4 +31,31 @@ app.command("/blackreaper-help", async ({ ack, respond }) => {
 /blackreaper-help - Show available commands
 /blackreaper-catfact - Get a cat fact`
   });
+});
+
+app.command("/blackreaper-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+app.command("/blackreaper-joke", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+    await respond({
+      text:
+`${response.data.setup}
+
+${response.data.punchline}`
+    });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a joke." });
+  }
 });
